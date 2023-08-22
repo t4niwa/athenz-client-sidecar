cc56c3735bec2d838d77372504a1470b2e1dcbf3
taniwa <taniwa@yahoo-corp.jp>
2023-08-22 10:47:46 +0900

    Add test

--- service/access.go
+++ service/access.go
@@ -249,7 +249,6 @@ func (a *accessService) getAccessToken(ctx context.Context, domain, role, proxyF
 	if !ok {
 		return a.updateAccessToken(ctx, domain, role, proxyForPrincipal, expiresIn)
 	}
-	// token_type is hardcoded in SIA. Because zts server hardcode token_type
 	atResponse := &AccessTokenResponse{
 		AccessToken: tok.token,
 		ExpiresIn:   int64(time.Unix(tok.expiry, 0).Sub(time.Now()).Seconds()),
